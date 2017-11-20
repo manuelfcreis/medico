a7c3f037719803bbaed3ee6aad07abbc57d73c4a
Ana Silva <ananunesdasilva@gmail.com>
2017-11-20 16:08:08 +0000

    controller for patient and doctor

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,3 +1,16 @@
 class DoctorsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  def index
+    @patients = Patient.all
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+  end
+
+  def edit
+  @doctor = Doctor.find(params[:id])
+  end
+
 end
