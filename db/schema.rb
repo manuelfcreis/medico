8bf11021d0a7ec553b0e670a33b1fe91f96b8803
Ana Silva <ananunesdasilva@gmail.com>
2017-11-23 13:45:58 +0000

    invitation to new user and existing user (patient) complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122153158) do
+ActiveRecord::Schema.define(version: 20171123120430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171122153158) do
     t.bigint "patient_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "accepted", default: false
     t.index ["doctor_id"], name: "index_chats_on_doctor_id"
     t.index ["patient_id"], name: "index_chats_on_patient_id"
   end
