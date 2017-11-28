0e261e4c3fa1d08a88c8a538e213bed4437c9730
Manuel CostaReis <manuelfcreis@gmail.com>
2017-11-28 18:15:13 +0000

    Changes all over the place

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -72,3 +72,9 @@ class PatientsController < ApplicationController
         )
     end
 end
+
+def Patients::InvitationsController < Devise::InvitationsController
+  def after_invite_path_for(resource)
+    settings_path
+  end
+end
