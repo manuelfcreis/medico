48f62e1939a9d58fd251914f2ef7ceaa2d937e4d
Marie-Charlotte Eyb <marie-charlotte@gmx.de>
2017-11-28 15:32:37 +0000

    message file connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include ApplicationHelper
+  skip_before_action :verify_authenticity_token 
   # before_action :authenticate_user!
   protected
   def authenticate_inviter!
